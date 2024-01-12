f2e1c884cbc72cbffc164871fd81ba1f6e2627e8
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-12 15:43:20 -0800

    Finalize setup for webpack

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hi!");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('hi!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoaSEnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
