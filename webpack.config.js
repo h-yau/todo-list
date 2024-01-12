63f4c658227cca60ac8f73490474fb42358bd32b
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-12 15:43:20 -0800

    Finalize setup for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean:true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
