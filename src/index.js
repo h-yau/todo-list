e3eb55b829ae19ecd14a290ab71cc8d53d1c7e9f
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-12 17:07:59 -0800

    Make modules to connect to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('What iss up!!!');
+// Notes:
+// need to implemenet -
+// 1. task (title, description, duedates, priorities)
+// 2. add new task
+// 3. mark as completed
+// 4. delete task
+// 5. store local Storage
+// perhaps use date-fns library to help manipulate times
+
+
+// Layout
+// 1. Have a title/ hero (tod-do list)
+// 2. right side is nav bar
+//     a. All Tasks
+//     b. Today's tasks
+//     c. This week
+//     d. important
+
+//     e. project -- add project
+// 3. main content to display whatever is rendered
+
+console.log('What is you!!!');
