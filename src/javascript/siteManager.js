a5738fcfe8a113417c4da5a25cd09586b927d3f0
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-12 17:20:56 -0800

    Rename main to siteManager and create logo

---
+++ src/javascript/siteManager.js
@@ -0,0 +1,11 @@
+function createLogo() {
+    const logo = document.createElement('div');
+    logo.textContent = 'To Do List';
+    document.body.appendChild(logo);
+    return
+}
+
+export default function prepareSite() {
+    createLogo();
+    
+}
