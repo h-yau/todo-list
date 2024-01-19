5f3eff89463f4d7dd01809411db7b1bd1b559db0
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-18 16:50:50 -0800

    Add more valition conditions

--- src/javascript/validateInput.js
+++ src/javascript/validateInput.js
@@ -1,4 +1,13 @@
 export default function validateTaskInputs(form) {
-    const title = form.querySelector('#title');
-    console.log(title.value);
+    const title = form.querySelector('#title').value;
+    const description = form.querySelector('#description').value;
+    const dueDate = form.querySelector('#due-date').value;
+    const isImportant = form.querySelector('#important').checked;
+    if (!title) {
+        console.error('It needs a title!');
+        return false
+    }
+    
+
+    return true
 }
