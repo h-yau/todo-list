d74b7135ee94fdab0b3f9928afff682d05bee08e
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-18 18:23:17 -0800

    Added refresh feature

--- src/javascript/content.js
+++ src/javascript/content.js
@@ -5,6 +5,34 @@ const testObject = todoObject("Run", "Run everyday!", "Tomorrow", "None");
 
 const addlist = (mainSection) => {
 
+    for (let i = 0; i < tasks.length; i++) {
+
+        const contentDiv = document.createElement('div');
+        contentDiv.classList.add('content-div');
+        mainSection.appendChild(contentDiv);
+    
+        const title = document.createElement('p');
+        title.classList.add('content-title');
+        title.textContent = tasks[i].getTitle();
+        contentDiv.appendChild(title);
+    
+        const desc = document.createElement('p');
+        desc.classList.add('content-desc');
+        desc.textContent = tasks[i].getDescription();
+        contentDiv.appendChild(desc);
+    
+        const dueDate = document.createElement('p');
+        dueDate.classList.add('content-due-date');
+        dueDate.textContent = tasks[i].getDueDate();
+        contentDiv.appendChild(dueDate);
+    
+        const priority = document.createElement('p');
+        priority.classList.add('content-priority');
+        priority.textContent = tasks[i].getPriority();
+        contentDiv.appendChild(priority);
+
+    }
+
     const contentDiv = document.createElement('div');
     contentDiv.classList.add('content-div');
     mainSection.appendChild(contentDiv);
--- src/javascript/tasksStorage.js
+++ src/javascript/tasksStorage.js
@@ -2,11 +2,5 @@ export const tasks = []
 
 export default function storeTask(task) {
     tasks.push(task);
-    tasks.forEach(item => {
-        ////// NEEDS edit so it doesn't just console, but to be stored
-        console.log(item.getTitle());
-        console.log(item.getDescription());
-        console.log(item.getDueDate());
-        console.log(item.getPriority());
-    })
+    console.table(tasks[0].getTitle());
 }
